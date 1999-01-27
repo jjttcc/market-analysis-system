cb835ee465aa1eab036e29f0cc2dfc5e69b600dd
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
1999-01-27 14:42:48 +0000

    Command-line client written in java

---
+++ src/clients/CL/CL_Client.java
@@ -0,0 +1,71 @@
+
+import java.io.*;
+import java.net.*;
+
+public class CL_Client
+{
+	public static void main(String[] args) throws IOException
+	{
+		Socket echoSocket = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+		String hname = "jupiter.milkyway.org";
+		Integer port_number = new Integer(22227);
+		char c;
+
+		if (args.length > 0)
+		{
+			hname = args[0];
+			if (args.length > 1)
+			{
+				port_number.parseInt (args[1]);
+			}
+		}
+
+		try
+		{
+			echoSocket = new Socket(hname, port_number.intValue());
+			out = new PrintWriter(echoSocket.getOutputStream(), true);
+			in = new BufferedReader(
+						new InputStreamReader(echoSocket.getInputStream()));
+		}
+		catch (UnknownHostException e)
+		{
+			System.err.println("Don't know about host:");
+			System.err.println(hname);
+			System.exit(1);
+		}
+		catch (IOException e)
+		{
+			System.err.println("Couldn't get I/O for the connection to:");
+			System.err.println(hname);
+			System.exit(1);
+		}
+
+		BufferedReader stdIn = new BufferedReader(
+									new InputStreamReader(System.in));
+		String userInput;
+		String serverOutput;
+
+		// Indicate to server that this is a command-line interface:
+		out.print("C");
+		out.flush();
+		do
+		{
+			do
+			{
+				c = (char) in.read();
+				if (c == '' || c == '') break;
+				System.out.print(c);
+			} while (true);
+			if (c == '') break;
+			userInput = stdIn.readLine();
+			out.println(userInput);
+		} while (true);
+
+		out.close();
+		in.close();
+		stdIn.close();
+		echoSocket.close();
+	}
+}
