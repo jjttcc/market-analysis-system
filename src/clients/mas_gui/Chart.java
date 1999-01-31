00c97d10285f4b3fb864f77f1cf77295f88c89d6
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
1999-01-31 06:13:56 +0000

    x

--- src/clients/mas_gui/Chart.java
+++ src/clients/mas_gui/Chart.java
@@ -13,7 +13,6 @@ public class TA_Chart extends Frame
 {
 	public TA_Chart(TA_Connection conn)
 	{
-
 		super("TA_Chart");		// Create the main window frame.
 		connection = conn;
 		num_windows++;			// Count it.
