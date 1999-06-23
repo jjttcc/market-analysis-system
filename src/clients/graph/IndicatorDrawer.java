1c9fd1ee90406d8a67082cb19b0499c78de01bc9
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
1999-06-23 22:16:01 +0000

    x

---
+++ src/clients/graph/IndicatorDrawer.java
@@ -0,0 +1,48 @@
+package graph;
+
+import java.awt.*;
+import java.util.*;
+import support.*;
+
+/**
+ *  Abstraction for drawing data tuples
+ */
+abstract public class IndicatorDrawer extends Drawer {
+
+	IndicatorDrawer(Drawer mktd) { _market_drawer = mktd; }
+
+	public int drawing_stride() { return 1; }
+
+	public Object data() { return _data; }
+
+	public String[] dates() { return _market_drawer.dates(); }
+
+	public Drawer market_drawer() { return _market_drawer; }
+
+	// Number of elements in the data
+	public int data_length() {
+		int result;
+		if (data() != null) {
+			result = _data.length;
+		} else {
+			result = 0;
+		}
+		return result;
+	}
+
+	public void set_dates(String[] d) { _indicator_dates = d; }
+
+	public void set_data(Object d) { _data = (double[]) d; }
+
+	protected int[] x_values() {
+		return _market_drawer.x_values();
+	}
+
+	protected int first_date_index() {
+		return Utilities.index_at_date(_indicator_dates[0], dates(), 1);
+	}
+
+	protected double _data[];
+	protected String[] _indicator_dates;
+	protected Drawer _market_drawer;
+}
---
+++ src/clients/graph/MarketDrawer.java
@@ -0,0 +1,49 @@
+package graph;
+
+import java.awt.*;
+import java.util.*;
+import support.*;
+
+/**
+ *  Drawer of market data
+ */
+abstract public class MarketDrawer extends Drawer {
+
+	// Number of fields in each tuple
+	public int drawing_stride() {
+		return Stride;
+	}
+
+	public Object data() { return _data; }
+
+	// The dates associated with the principle (market) data
+	public String[] dates() { return _dates; }
+
+	public Drawer market_drawer() { return this; }
+
+	// Number of elements in the data
+	public int data_length() {
+		int result;
+		if (data() != null) {
+			result = _data.length;
+		} else {
+			result = 0;
+		}
+		return result;
+	}
+
+	// Set the dates.
+	public void set_dates(String[] d) { _dates = d; }
+
+	// Set data to `d'.
+	public void set_data(Object d) { _data = (double[]) d; }
+
+	protected int[] x_values() {
+		return _x_values;
+	}
+
+	protected int _x_values[];
+	protected double _data[];
+	protected String[] _dates;
+	protected final int Stride = 4;
+}
