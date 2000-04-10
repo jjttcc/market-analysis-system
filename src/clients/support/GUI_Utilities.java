0b18314d54c4b8400ec2a09e169d41831c2b0c20
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2000-04-10 03:22:56 +0000

    x

---
+++ src/clients/support/GUI_Utilities.java
@@ -0,0 +1,16 @@
+/* Copyright 1998 - 2000: Jim Cochrane and others - see file forum.txt */
+
+package support;
+
+import java.awt.*;
+
+public class GUI_Utilities extends Component {
+	static public void busy_cursor(boolean on, Component c) {
+		if (on) {
+			c.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+		}
+		else {
+			c.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		}
+	}
+}
