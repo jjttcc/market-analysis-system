d15d8565245320dae25a47303b0fc785832e5fce
jcochrane <jcochrane@8105f137-5510-0410-97d7-f6e47f13c945>
2000-10-02 23:30:50 +0000

    Changed to use new bar_width feature to make bars same width as the main data bars.

--- src/clients/graph/BarDrawer.java
+++ src/clients/graph/BarDrawer.java
@@ -25,7 +25,7 @@ public class BarDrawer extends IndicatorDrawer {
 		if (_data != null) lngth = data_length();
 		if (lngth == 0) return;
 
-		int bar_width = (int) ((double) base_bar_width(bounds, lngth) * .6);
+		int _bar_width = bar_width();
 		double height_factor;
 		int x_s[] = new int[4], y_s[] = new int[4];
 		Configuration conf = Configuration.instance();
@@ -36,15 +36,15 @@ public class BarDrawer extends IndicatorDrawer {
 		// will be called before any data has been placed in the class.
 		if (_data == null || lngth < 1) return;
 
-		if (bar_width <= 0) bar_width = 1;
+		if (_bar_width <= 0) _bar_width = 1;
 		g.setColor(draw_color);
 		height_factor = height_factor_value(bounds);
 		row = first_row() - 1;
-		for (i = 0; i < lngth; ++i, ++row) {
+		for (i = 0; row < lngth; ++i, ++row) {
 			 x = _x_values[row] + 1;
 			y = (int)(bounds.height - (_data[i]-ymin) * height_factor +
 					bounds.y);
-			x_s[0] = x; x_s[1] = x + bar_width;
+			x_s[0] = x; x_s[1] = x + _bar_width;
 			x_s[2] = x_s[1]; x_s[3] = x_s[0];
 			y_s[0] = y; y_s[1] = y;
 			y_s[2] = bounds.height; y_s[3] = bounds.height;
