7a95ca8940b722d836965a838d69c1ef680e5513
jcochrane <jcochrane@8105f137-5510-0410-97d7-f6e47f13c945>
2000-10-02 23:23:06 +0000

    New times/set_times, bar_width implementation.  Expanded first_date_index to handle times for intraday data.

--- src/clients/graph/IndicatorDrawer.java
+++ src/clients/graph/IndicatorDrawer.java
@@ -19,6 +19,8 @@ abstract public class IndicatorDrawer extends Drawer {
 
 	public String[] dates() { return _market_drawer.dates(); }
 
+	public String[] times() { return _market_drawer.times(); }
+
 	public Drawer market_drawer() { return _market_drawer; }
 
 	// Number of elements in the data
@@ -34,17 +36,50 @@ abstract public class IndicatorDrawer extends Drawer {
 
 	public void set_dates(String[] d) { _indicator_dates = d; }
 
+	public void set_times(String[] t) { _indicator_times = t; }
+
 	public void set_data(Object d) { _data = (double[]) d; }
 
 	protected int[] x_values() {
 		return _market_drawer.x_values();
 	}
 
+	protected int bar_width() {
+		return _market_drawer.bar_width();
+	}
+
 	protected int first_date_index() {
-		return Utilities.index_at_date(_indicator_dates[0], dates(), 1);
+		int result;
+		String[] dates = dates();
+		String[] times = times();
+
+		result = Utilities.index_at_date(_indicator_dates[0], dates, 1, 0,
+			dates.length - 1);
+		if (result > 0 &&
+				_indicator_times != null && _indicator_times.length > 0) {
+			// Since the data is intraday, there are duplicate dates in
+			// the dates array.  Find the first element of dates that
+			// matches _indicator_dates[0].
+			while (result > 0 && dates[result-1].equals(_indicator_dates[0])) {
+				--result;
+			}
+			if (! _indicator_times[0].equals(times[result])) {
+				// If _indicator_times[0] is less than times[result],
+				// increment result until times[result] is the first time
+				// of the following day.
+				while (result < times.length &&
+						_indicator_times[0].compareTo(times[result]) < 0) {
+					++result;
+				}
+				result = Utilities.index_at_date(_indicator_times[0], times,
+					1, result, times.length - 1);
+			}
+		}
+		return result;
 	}
 
 	protected double _data[];
 	protected String[] _indicator_dates;
+	protected String[] _indicator_times;
 	protected Drawer _market_drawer;
 }
