ca112f33a336a353c23fd003113a8fb974058ae0
jcochrane <jcochrane@8105f137-5510-0410-97d7-f6e47f13c945>
2000-12-23 22:10:39 +0000

    Added more select_patterns to cover missed cases (objects were not being identified and so the number used to select the object was used instead). Bug fix in record_input - a case was missed.  Put print statements back in (commented-out), since they are helpful in bug hunting.

--- src/scripts/python/mas/client/CommandProcessor.py
+++ src/scripts/python/mas/client/CommandProcessor.py
@@ -10,9 +10,13 @@ class CommandProcessor:
 		self.fatal_error = false
 		self.error = false
 		self.invalid_pattern = "Invalid selection"
-		self.select_patterns = ["Select[^?]*function",
-			"Select[^?]*opera[nt][do]",
-			"Select the technical indicator"]
+		self.select_patterns = ["\n*Select[^?]*function",
+			"\n*Select[^?]*opera[nt][do]",
+			"\n*Select *an *indicator *for.*'s",
+			"\n*Select *an *object *for.*'s",
+			"\n*Select the technical indicator",
+			"\n*Select *an *indicator *to *edit:",
+			"1) "]
 		self.non_shared_pattern = ".*List of all valid objects:.*"
 		self.objects = {}
 		self.shared_objects = {}
@@ -42,25 +46,36 @@ class CommandProcessor:
 		key_matched = false
 		if match(self.shared_pattern, response) != -1:
 			otable = self.shared_objects
+#			print "Using shared list"
+#			print otable.items()
 			response = sub(self.shared_pattern, "", response)
 			shared = true
 		else:
 			otable = self.objects
+#		print "Checking '" + response + "' with:\n"
+#		print otable.items()
 		if self.selection and otable.has_key(response):
 			self.result = otable[response]
+#			print "Matched: " + self.result
 			key_matched = true
 		else:
 			self.result = response
+#			print "No match: " + self.result
 		if self.record:
 			self.record_input(self.result, shared, key_matched)
 		self.result = self.result + '\n'
 
 	def select_object_match(self, s):
 		result = false
+#		print "Checking for match of " + s
 		for pattern in self.select_patterns:
+#			print "with " + pattern
 			if match(pattern, s) != -1:
 				result = true
 				break
+#		print "returning result of ",
+#		if result: print "true"
+#		else: print "false"
 		return result
 
 	def store_choices(self, s):
@@ -76,8 +91,13 @@ class CommandProcessor:
 					len(self.objects) > 0:
 				self.shared_objects = self.objects.copy()
 				self.objects.clear()
+#		print "<<l, shared objects>>: " + l
+#		print self.shared_objects.items()
+#		print "Stored: " + self.objects[objname] + " (" + objname + ")"
 
 	def record_input(self, s, shared, key_match):
+#		print "ri - s, shared, key_match: '" + s + "', " + `shared` + \
+#			", " + `key_match`
 		if self.selection:
 			if key_match:
 				if shared:
@@ -86,12 +106,18 @@ class CommandProcessor:
 				else:
 					self.input_record = self.input_record + key + '\n'
 			elif s in self.objects.values():
+#				print s + " in objects"
 				self.input_record = self.input_record + \
 					self.key_for(s, self.objects) + '\n'
 			elif s in self.shared_objects.values():
+#				print s + " in shared objects"
 				self.input_record = self.input_record + self.shared_string + \
 					self.key_for(s, self.shared_objects) + '\n'
+			else:
+#				print "adding " + s + " to input record"
+				self.input_record = self.input_record + s + '\n'
 		else:
+#			print "adding " +  self.result + " to input record"
 			self.input_record = self.input_record + self.result + '\n'
 
 	def key_for(self, s, objects):
@@ -99,4 +125,5 @@ class CommandProcessor:
 		for k in objects.keys():
 			if objects[k] == s:
 				result = k
+#		print "key_for returning " + result
 		return result
