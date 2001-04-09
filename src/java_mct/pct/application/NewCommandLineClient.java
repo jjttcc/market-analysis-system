098dd1e5ceb5d217e8fb7009c70547528bc8e72d
jcochrane <jcochrane@8105f137-5510-0410-97d7-f6e47f13c945>
2001-04-09 23:49:12 +0000

    starting to use new "settings" table

--- src/java_mct/pct/application/NewCommandLineClient.java
+++ src/java_mct/pct/application/NewCommandLineClient.java
@@ -3,7 +3,7 @@ package pct.application;
 import java.io.*;
 
 // Plug-in to start a new MAS command-line client process
-public class NewCommandLineClient {
+public class NewCommandLineClient implements MCT_Constants {
 	public NewCommandLineClient(Object pcontext) {
 		parent_context = (MCT_ComponentContext) pcontext;
 	}
@@ -14,6 +14,9 @@ public class NewCommandLineClient {
 			runtime = Runtime.getRuntime();
 		}
 		try {
+System.out.println("NCLC - " + command_line_cmd_key + ": " +
+settings.get(command_line_cmd_key));
+System.out.println("settings: " + settings);
 String tmp_cmd = "xterm -e /opt/mas/bin/macl -h " + " " +
 parent_context.server_host_name() + " " +
 parent_context.server_port_number();
