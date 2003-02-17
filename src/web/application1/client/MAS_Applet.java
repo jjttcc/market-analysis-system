4efcb5792a2129fafb8c512e05ac5408f13abc6d
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2003-02-17 07:32:53 +0000

    Copied some initialization code from MA_Client to here and adapted to work as an applet.  It works!

--- src/web/application1/client/MAS_Applet.java
+++ src/web/application1/client/MAS_Applet.java
@@ -8,75 +8,84 @@ import java.awt.*;
 import java.net.*;
 import java.io.*;
 import mas_gui.*;
+import support.IO_URL_Connection;
+import support.Configuration;
 
 // Test applet that communicates with the servlet via serialization
 public class MAS_Applet extends Applet {
-	public void paint(Graphics g) {
-		if (logMsg != null) {
-			g.drawString(logMsg, 5, 50);
-		}
-	}
 
 	public void init() {
+		log("init: Starting ...");
+		log("Compiled at Sun Feb 16 17:55:29 MST 2003");
+		MAS_Options options = new AppletOptions();
 		try {
-			initialize();
+			log("init: A");
+			initialize_server_address();
+			log("init: B");
 			Chart chart;
-//!!!Stub:
-			Connection c = null;
-			DataSetBuilder data_builder = new mas_gui.DataSetBuilder(null,null);
-			chart = new mas_gui.Chart(data_builder, chart_filename, null);
+			// Can't read files from an applet.
+			Configuration.set_use_config_file(false);
+			log("init: C");
+			DataSetBuilder data_builder = new DataSetBuilder(connection(),
+				options);
+			log("init: D");
+			chart = new Chart(data_builder, null, options);
+			log("init: E");
 		} catch (Exception e) {
 			log("Connection failed: " + e.toString());
+			destroy();
 		}
+		log("init: Exiting ...");
 	}
 
-//!!!!!!!Temporary - need to fix:
-private static final String chart_filename = ".ma_client_settings";
+	private Connection connection() throws Exception {
+		assert server_address != null;
+		Connection result;
+
+		// The applet client uses an URL connection to talk to the server.
+		IO_URL_Connection io_connection =
+			new IO_URL_Connection(server_address);
+		if (compression()) {
+			result = new CompressedConnection(io_connection);
+		} else {
+			result = new Connection(io_connection);
+		}
+
+		return result;
+	}
 
 // Implementation - initialization
 
-	// Postcondition: hostName != null && serverAddress != null &&
-	//    port > 0 && connection != null
-	private void initialize() throws Exception {
-		URL hostURL = getCodeBase();
-		hostName = hostURL.getHost();
-		port = hostURL.getPort();
-		if (port == -1)
-		{
+	// Postcondition: host_name != null && server_address != null && port > 0
+	private void initialize_server_address() throws Exception {
+		URL host_url = getCodeBase();
+		host_name = host_url.getHost();
+		port = host_url.getPort();
+		if (port == -1) {
 			port = 80;
 		}
-		serverAddress = "http://" + hostName + ":" + port + servletPath;
-		connection = serverConnection();
-		assert hostName != null && serverAddress != null && port > 0 &&
-			connection != null;
+		server_address = "http://" + host_name + ":" + port + servlet_path;
+		assert host_name != null && server_address != null && port > 0;
 	}
 
 // Implementation - utilities
 
-	// Connection to the server for binary input and ouput with caching off
-	// Precondition: serverAddress != null
-	private URLConnection serverConnection() throws Exception {
-		URLConnection result = (new URL(serverAddress)).openConnection();
-
-		result.setDoInput(true);
-		result.setDoOutput(true);
-		result.setUseCaches (false);
-		result.setRequestProperty ("Content-Type",
-			"application/octet-stream");
-		return result;
+	private void log(String msg) {
+		showStatus(msg);
+		System.out.println(msg + "\n");
 	}
 
-	private void log(String msg) {
-		logMsg = msg;
+	private boolean compression() {
+		// @@For now, default to no compression.
+		return false;
 	}
 
 // Implementation - attributes
 
-	private String hostName = "";
+	private String host_name = "";
 	private int port = -1;
-	private String logMsg;
-	private URLConnection connection;
-
-	private String servletPath = "/mas/mas";
-	private String serverAddress = null;
+	private String log_msg;
+	//!!!!Needs to be made configurable:
+	private String servlet_path = "/mas/mas";
+	private String server_address = null;
 }
