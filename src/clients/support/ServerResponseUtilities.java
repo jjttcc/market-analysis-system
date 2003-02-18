a6088b18e92d452fc89684f82aa892eeb93dd169
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2003-02-18 01:24:00 +0000

    Allows the servlet to send proper (MAS GUI protocol) responses to the client applet.

---
+++ src/clients/support/ServerResponseUtilities.java
@@ -0,0 +1,26 @@
+/* Copyright 1998 - 2003: Jim Cochrane - see file forum.txt */
+
+package support;
+
+//import java.util.*;
+//import java.text.*;
+//import java.io.*;
+//import common.Constants;
+import common.NetworkProtocol;
+
+/** Utilities for server responses to the GUI client */
+public class ServerResponseUtilities implements NetworkProtocol {
+
+	public ServerResponseUtilities() {}
+
+	// Response to the client, built with 'msg_id' and 'msg'
+	public static String response_message(int msg_id, String msg) {
+		return new Integer(msg_id).toString() + Output_field_separator +
+			msg + Eom;
+	}
+
+	// Error-status response to the client, built with 'msg'
+	public static String error_response_message(String msg) {
+		return response_message(Error, msg);
+	}
+}
