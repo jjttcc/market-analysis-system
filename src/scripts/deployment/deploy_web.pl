0e1c4518d0759be86a83cf3cbd23da5acfd910e2
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2003-03-10 23:01:17 +0000

    Completed the applet/server deployment functionality, including updating the port # to the configured setting.

--- src/scripts/deployment/deploy_web.pl
+++ src/scripts/deployment/deploy_web.pl
@@ -5,8 +5,8 @@
 # revision: "$Revision$"
 
 use deploy ('configure', 'process_args', 'setup', 'cleanup',
-	'applet_archive', 'servlet_archive', 'applet_deployment_directory',
-	'servlet_deployment_directory'
+	'applet_archive', 'servlet_archive', 'applet_deployment_path',
+	'servlet_deployment_directory', 'mas_port'
 );
 
 # Constants
@@ -15,28 +15,68 @@ use deploy ('configure', 'process_args', 'setup', 'cleanup',
 &configure;
 &setup;
 &install_applet;
-exit;
 &install_servlet;
-# No cleanup needed.
+&configure_servlet;
+&cleanup;
 
 ### Main operations
 
 # Intall the applet into the location specified by the configuration file.
 sub install_applet {
 	print "Installing applet.\n";
+	# Make sure the archive file exists and is readable.
+	open(F, &applet_archive) || die "Cannot open file: " .
+		&applet_archive .  " for reading.\n";
+	close(F);
+	print "Unpacking " . &applet_archive. "\n";
+	! system("cp " . &applet_archive . " " . &applet_deployment_path) ||
+		die "Failed to copy " . &applet_archive . " to " .
+		&applet_deployment_path . "\n";
 }
 
 # Intall the servlet into the location specified by the configuration file.
 sub install_servlet {
 	print "Installing servlet.\n";
+	chdir &servlet_deployment_directory ||
+		die "Could not cd to ", &servlet_deployment_directory, "\n";
 	# Make sure the archive file exists and is readable.
 	open(F, &servlet_archive) || die "Cannot open file: " .
-		&servlet_archive .  "for reading.\n";
+		&servlet_archive . " for reading.\n";
 	close(F);
-	print "Untarring " . &servlet_archive. "\n";
-	! system("jar zxf " . &servlet_archive) || die "Failed to untar " .
+	print "Unpacking " . &servlet_archive. "\n";
+	! system("jar xvf " . &servlet_archive) || die "Failed to unpack " .
 		&servlet_archive . "\n";
 }
 
-### General utilities
-
+# Configure settings for the servlet.
+sub configure_servlet {
+	my $config_file = &servlet_deployment_directory . "/WEB-INF/web.xml";
+	my $port_tag = "mas-server-port";
+	my $port = &mas_port;
+	open(F, $config_file) || die "Cannot open file: " .
+		$config_file . " for reading.\n";
+	my @contents = <F>;
+	my @new_contents = ();
+	my $port_changed = 0;
+	my $i = 0;
+	while ($i < @contents) {
+		if ($contents[$i] =~ /<param-name>$port_tag<\/param-name>$/) {
+			push(@new_contents, $contents[$i]);
+			++$i;
+			if ($contents[$i] =~ /<param-value>[0-9]+<\/param-value>/) {
+				$contents[$i] =~
+					s/(<param-value>)[0-9]+(<\/param-value>)/$1$port$2/;
+				$port_changed = 1;
+			}
+		}
+		push(@new_contents, $contents[$i]);
+		++$i;
+	}
+	if (! $port_changed) {
+		print "Warning: $port_tag parameter not found in:\n$config_file.\n";
+	}
+	close(F);
+	open(F, "> " . $config_file) || die "Cannot open file: " .
+		$config_file . " for writing.\n";
+	print F @new_contents;
+}
