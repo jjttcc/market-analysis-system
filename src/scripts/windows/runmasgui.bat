dbadf6e4de946eee3634b236d5c989dfa7494333
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2003-06-04 16:26:08 +0000

    Changed to take the port number as an argument, if supplied.

--- src/scripts/windows/runmasgui.bat
+++ src/scripts/windows/runmasgui.bat
@@ -1,5 +1,10 @@
+echo off
 rem Run the mas GUI - connect with the server started with runmas
+rem If a parameter is passed in, it will be used as the port number;
+rem otherwise a hard-coded number will be used.
 
+set port=18273
+if not "%1"=="" set port=%1
 cd %MAS_DIRECTORY%
 cd classes
-java MA_Client localhost 18273
+java MA_Client localhost %port%
