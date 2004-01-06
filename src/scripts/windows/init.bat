355e94973b813217ec90f079d5913acf1e1e8607
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2004-01-06 19:02:51 +0000

    work on passing args with spaces

--- src/scripts/windows/init.bat
+++ src/scripts/windows/init.bat
@@ -1,6 +1,7 @@
 echo init.bat is running in:>repl_spec
+rem set line_=%line_:"=%
 cd>>repl_spec
 echo replacestart>>repl_spec
-echo {application_directory}%1>>repl_spec
+echo {application_directory}"%1">>repl_spec
 echo replaceend>>repl_spec
 :end
