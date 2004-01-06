29007e84b6426ac5360455c524146463feca29a4
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2004-01-06 23:28:02 +0000

    Getting closer to setting up a config file for and running the config_tool to create a mctrc file for the user's system.

--- src/scripts/windows/init.bat
+++ src/scripts/windows/init.bat
@@ -1,2 +1,7 @@
 @echo off
+rem bash complains if it can't find a /tmp directory:
+set maketmp=n
+if not exist \tmp set maketmp=y
+if %maketmp% == y md \tmp
 .\bash bash_init %1 %2 %3 %4 %5 %6 %7 %8 %9
+if %maketmp% == y rmdir \tmp
