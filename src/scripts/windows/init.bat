498db5fa7ef8b98b19ce818293a7f2da17ea0528
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2004-01-08 04:21:57 +0000

    spello correction

--- src/scripts/windows/init.bat
+++ src/scripts/windows/init.bat
@@ -3,5 +3,5 @@ rem bash complains if it can't find a /tmp directory:
 set maketmp=n
 if not exist \tmp set maketmp=y
 if %maketmp% == y md \tmp
-%comspect% /c start /min .\bash bash_init %1 %2 %3 %4 %5 %6 %7 %8 %9
+%comspec% /c start /min .\bash bash_init %1 %2 %3 %4 %5 %6 %7 %8 %9
 if %maketmp% == y rmdir \tmp
