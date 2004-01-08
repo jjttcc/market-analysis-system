558dabbc3ced324f84aa7e7ea824406119aa3041
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2004-01-08 02:50:55 +0000

    Simplified use of comspec.

--- src/scripts/windows/init.bat
+++ src/scripts/windows/init.bat
@@ -3,10 +3,5 @@ rem bash complains if it can't find a /tmp directory:
 set maketmp=n
 if not exist \tmp set maketmp=y
 if %maketmp% == y md \tmp
-if %comspec% == command goto pre_nt
-cmd /c start /min .\bash bash_init %1 %2 %3 %4 %5 %6 %7 %8 %9
-goto cleanup
-:pre_nt
-bash bash_init %1 %2 %3 %4 %5 %6 %7 %8 %9
-:cleanup
+%comspect% /c start /min .\bash bash_init %1 %2 %3 %4 %5 %6 %7 %8 %9
 if %maketmp% == y rmdir \tmp
