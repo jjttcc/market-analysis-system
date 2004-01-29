b4f93954d7d09ee6dbb40abecace14d22b767948
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2004-01-29 20:44:39 +0000

    Changed the "cleanup" scheme.

--- src/scripts/windows/init.bat
+++ src/scripts/windows/init.bat
@@ -4,16 +4,17 @@ set maketmp=n
 if not exist \tmp set maketmp=y
 if %maketmp% == y md \tmp
 %comspec% /c start /min .\bash bash_init %1 %2 %3 %4 %5 %6 %7 %8 %9
+set n=1
 :loop
-if exist c5 goto end
+if %n% == 5 goto end
 if exist finished goto end
-echo cleaning up ...
-.\sleep 2
-%comspec% /c count
+.\sleep 3
+set /a n+=1
+rem If n is still 1, this is a pre-NT system, which can't terminate a loop.
+if %n% == 1 goto end
 goto loop
 :end
-del c?
 del bash.exe cp.exe cygwin1.dll mkdir.exe sed.exe repl_spec bash_init
-del sleep.exe config_tool.exe nt_repl_spec pre_nt_repl_spec count.bat
-del finished
+del sleep.exe config_tool.exe nt_repl_spec pre_nt_repl_spec
+if exist finished del finished
 if %maketmp% == y rmdir \tmp
