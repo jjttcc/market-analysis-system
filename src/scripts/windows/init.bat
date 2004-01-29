4611a7038d0fea7d5f40a9b592b927548db1fac5
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2004-01-29 17:48:00 +0000

    initial changes to "clean up"

--- src/scripts/windows/init.bat
+++ src/scripts/windows/init.bat
@@ -4,4 +4,6 @@ set maketmp=n
 if not exist \tmp set maketmp=y
 if %maketmp% == y md \tmp
 %comspec% /c start /min .\bash bash_init %1 %2 %3 %4 %5 %6 %7 %8 %9
+rem clean up
+del bash.exe cp.exe cygwin1.dll mkdir.exe sed.exe repl_spec
 if %maketmp% == y rmdir \tmp
