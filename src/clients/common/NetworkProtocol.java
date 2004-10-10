eeb1446910c01e17ade2bac18bb9490a14c46540
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2004-10-10 05:33:46 +0000

    Updated re. protocol extensions in the server.

--- src/clients/common/NetworkProtocol.java
+++ src/clients/common/NetworkProtocol.java
@@ -8,36 +8,36 @@ public interface NetworkProtocol
 {
 
 	// Client request IDs
-	final int Market_data_request = 1;
-	final int Indicator_data_request = 2;
-	final int Trading_period_type_request = 3;
-	final int Market_list_request = 4;
-	final int Indicator_list_request = 5;
-	final int Login_request = 6;
-	final int Session_change_request = 7;
-	final int Logout_request = 8;
-	final int Event_data_request = 9;
-	final int Event_list_request = 10;
-	final int All_indicators_request = 11;
+	final int market_data_request = 1;
+	final int indicator_data_request = 2;
+	final int trading_period_type_request = 3;
+	final int market_list_request = 4;
+	final int indicator_list_request = 5;
+	final int login_request = 6;
+	final int session_change_request = 7;
+	final int logout_request = 8;
+	final int event_data_request = 9;
+	final int event_list_request = 10;
+	final int all_indicators_request = 11;
 
 	// Server response IDs
-	final int Error = 101;
-	final int OK = 102;
-	final int Invalid_symbol = 103;
-	final int Warning = 104;
-	final int Invalid_period_type = 105;
+	final int error = 101;
+	final int ok = 102;
+	final int invalid_symbol = 103;
+	final int warning = 104;
+	final int invalid_period_type = 105;
 
 	// Server response strings
-	final String No_open_session_state = "no_open";
-	final String Open_interest_flag = "oi";
+	final String no_open_session_state = "no_open";
+	final String open_interest_flag = "oi";
 
 	// Subtokens
-	final String Start_date = "start_date";
-	final String End_date = "end_date";
+	final String start_date = "start_date";
+	final String end_date = "end_date";
 
 	// Field separators
-	final String Message_date_field_separator = "";
-	final String Message_time_field_separator = "";
+	final String message_date_field_separator = "";
+	final String message_time_field_separator = "";
 
 	// String constants
 	final String Eom = "";
@@ -46,6 +46,16 @@ public interface NetworkProtocol
 	final String Message_field_separator = "\t";
 	final String Message_record_separator = "\n";
 
+	// Client request IDs
+	final int time_delimited_market_data_request = 12;
+	final int time_delimited_indicator_data_request = 13;
+
 	// daily period type is needed for initialization.
 	final String daily_period_type = "daily";
+
+	// Field separators for date-time components of client requests
+	final String client_request_date_field_separator = "/";
+	final String client_request_time_field_separator = ":";
+	final String client_request_date_time_range_separator = ";";
+	final String client_request_date_time_separator = ",";
 }
