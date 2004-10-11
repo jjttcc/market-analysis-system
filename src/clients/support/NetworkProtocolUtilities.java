9055b9b0f60a668605e40ac27a44cb9d9edce92e
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2004-10-11 21:26:10 +0000

    Implemented 'one_second_later' and, using the new features availabe in DateTimeServices, 'date_time_range'.

--- src/clients/support/NetworkProtocolUtilities.java
+++ src/clients/support/NetworkProtocolUtilities.java
@@ -4,27 +4,46 @@ package support;
 
 import java.awt.*;
 import common.NetworkProtocol;
-import java.util.Date;
+import java.util.*;
+import java_library.support.*;
 
 
 // Utilities that aid in communicating with the server according to the
 // network protocol specification
-public class NetworkProtocolUtilities implements NetworkProtocol {
+public class NetworkProtocolUtilities extends DateTimeServices
+	implements NetworkProtocol {
 
 	// The date-time range, formatted according to the protocol, for a
 	// time-delimited data request starting at `start' and ending at `end'
-	public String date_time_range(Date start, Date end) {
-return ""; //!!!!To be implemented
+	public String date_time_range(Calendar start, Calendar end) {
+		String start_string = standard_formatted_date(start,
+			Client_request_date_field_separator) +
+			Client_request_date_time_separator +
+			standard_formatted_time(start, Client_request_time_field_separator);
+		String end_string = standard_formatted_date(end,
+			Client_request_date_field_separator) +
+			Client_request_date_time_separator +
+			standard_formatted_time(end, Client_request_time_field_separator);
+		return start_string +
+			Client_request_date_time_range_separator + end_string;
 	}
 
 //!!!Needed:?
-	public Date date_from_string(Date d) {
+	public Calendar date_from_string(Calendar date) {
 return null; //!!!!To be implemented
 	}
 
-	// A date-time one second later than `d'
-	// Preconditiion: d != null
-	public Date one_second_later(Date d) {
-		return new Date(d.getTime() + 1);
+	// A date-time one second later than `date'
+	// Preconditiion: date != null
+	public Calendar one_second_later(Calendar date) {
+		int y, m, d, h, min, s;
+		y = date.get(date.YEAR);
+		m = date.get(date.MONTH);
+		d = date.get(date.DATE);
+		h = date.get(date.HOUR_OF_DAY);
+		min = date.get(date.MINUTE);
+		s = date.get(date.SECOND);
+
+		return new GregorianCalendar(y, m, d, h, min, s);
 	}
 }
