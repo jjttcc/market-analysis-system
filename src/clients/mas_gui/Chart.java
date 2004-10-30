81f3cf94524600d8083a437e4ad20e08475a8a7c
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2004-10-30 06:51:14 +0000

    more cleanup activities

--- src/clients/mas_gui/Chart.java
+++ src/clients/mas_gui/Chart.java
@@ -431,12 +431,14 @@ public class Chart extends Frame implements Runnable, NetworkProtocol,
 	}
 
 	public void notify_of_failure(Exception e) {
-		// Report the failure and ???
+		// @@Report the failure and then what???
+		display_warning("data request failed [Trace information: " +
+			e + "]");
 	}
 
 	public void notify_of_error(int result_id, String msg) {
-System.out.println("data request failed with code: " + result_id);
-		// Report the error and ???
+		display_warning("data request failed with code: " + result_id +
+			", " + msg);
 	}
 
 // Basic operations - package access
@@ -679,12 +681,7 @@ System.out.println("data request failed with code: " + result_id);
 		// If the current upper and lower indicator lists are not empty,
 		// they were loaded from the saved settings.  Make sure they
 		// get marked as "selected".
-//!!!:
-System.out.println("from the settings, selecting upper indicators: " +
-current_upper_indicators);
 		tradable_specification.select_indicators(current_upper_indicators);
-System.out.println("from the settings, selecting lower indicators: " +
-current_lower_indicators);
 		tradable_specification.select_indicators(current_lower_indicators);
 	}
 
