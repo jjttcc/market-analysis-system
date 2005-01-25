a27160aac87c1e0906bd72bc5b223c44169a5472
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2005-01-25 08:49:26 +0000

    A new debugging tools class.

---
+++ src/clients/common/DebuggingUtilities.java
@@ -0,0 +1,10 @@
+/* Copyright 1998 - 2004: Jim Cochrane - see file forum.txt */
+
+package common;
+
+/** Debugging utiities */
+public class DebuggingUtilities {
+
+	// Should stack a trace be printed as part of some debugging reports?
+	public static boolean print_stack_trace = false;
+}
