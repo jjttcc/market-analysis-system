2d84ef8ec8072b7491e0e7d066a242b7fd42a8e7
jtc <jtc@8105f137-5510-0410-97d7-f6e47f13c945>
2005-03-07 00:26:10 +0000

    Added new protocol components, borrowed from mas eiffel code.

--- src/scripts/test_scripts/data_supplier/application_library/support/CommunicationProtocol.pm
+++ src/scripts/test_scripts/data_supplier/application_library/support/CommunicationProtocol.pm
@@ -36,4 +36,26 @@ Protocol for communication with the client
 	# Error status_id
 	use constant error_id => '2';
 
+	# Character used to separate top-level message components
+	use constant message_component_separator => "%T";
+
+	# Character used to separate "records" or "lines" within
+	# a message component
+	use constant message_record_separator => "%N";
+
+	# Default field separator of date fields from input
+	use constant date_field_separator => "/";
+
+	# Default field separator of time fields from input
+	use constant time_field_separator => ":";
+
+	# Default character used to separate the start-date-time field
+	# from the end-date-time field in a date-time range
+	# specification
+	use constant date_time_range_separator => ";";
+
+	# Default character used to separate the date field from
+	# the time field
+	use constant date_time_separator => ",";
+
 1;
